96877c45b9fe199f3e48a3ce247c750f8789de8e
ChulWoong Kim <chulwoong.kim@gmail.com>
2017-08-23 17:04:58 +0900

    Initial Commit

---
+++ IRLibrary Core Data/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IRLibrary Core Data
+//
+//  Created by 김철웅 on 2017. 8. 23..
+//  Copyright © 2017년 IRON Private Firm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
